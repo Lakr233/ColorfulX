3ed67dfb7b3e888ce1e4272d1a2d7e699c6d1878
Lakr <25259084+Lakr233@users.noreply.github.com>
2024-07-04 22:26:31 +0800

    Remove Print

--- Sources/ColorfulX/Color.swift
+++ Sources/ColorfulX/Color.swift
@@ -26,7 +26,6 @@ public typealias ColorSpace = ColorVector.Space
 
 extension ColorVector {
     init(_ color: ColorElement, usingSpace space: Space = .rgb) {
-        print("color: \(color)")
         let cgColor = color.cgColor
         let color = cgColor.converted(
             to: CGColorSpace(name: CGColorSpace.sRGB)!,
