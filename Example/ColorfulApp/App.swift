8792750494245cee87eb7a50fdf4ec8cb3188a84
砍砍 <git@qaq.wiki>
2024-08-29 15:00:30 +0800

    Update Demo App @ Catalyst

--- Example/ColorfulApp/App.swift
+++ Example/ColorfulApp/App.swift
@@ -9,14 +9,35 @@ import ColorfulX
 import SwiftUI
 
 struct App: SwiftUI.App {
+    #if targetEnvironment(macCatalyst)
+        @Environment(\.scenePhase) var scenePhase
+    #endif
+
     var body: some Scene {
         WindowGroup {
             ContentView().background(Color.white)
         }
+        #if targetEnvironment(macCatalyst)
+        .onChange(of: scenePhase) { _ in
+            removeTitleBarFromWindow()
+        }
+        #endif
         #if os(macOS)
         .windowResizability(.contentSize)
         .windowStyle(.hiddenTitleBar)
         .windowToolbarStyle(.unifiedCompact)
         #endif
     }
+
+    #if targetEnvironment(macCatalyst)
+        func removeTitleBarFromWindow() {
+            let scenes = UIApplication.shared.connectedScenes.map { $0 as? UIWindowScene }
+            for scene in scenes {
+                if let titlebar = scene?.titlebar {
+                    titlebar.titleVisibility = .hidden
+                    titlebar.toolbar = nil
+                }
+            }
+        }
+    #endif
 }
